935176ea4ff54020737ff1812cb71e8c22c61908
Al.Romanov <raa_2001@mail.ru>
2021-12-11 23:40:54 +0300

    Done

---
+++ gausslikeqf.py
@@ -0,0 +1,25 @@
+import math
+
+def gauss_qf2(f, a, b, moments):
+    ms = [moment(b) - moment(a) for moment in moments]
+    print("Моменты весовой функции")
+    for i in range(len(ms)):
+        print("\tMu{} = {}".format(i, ms[i]))
+    a1 = (ms[0] * ms[3] - ms[2] * ms[1]) / (ms[1] * ms[1] - ms[2] * ms[0])
+    a2 = (ms[2] * ms[2] - ms[3] * ms[1]) / (ms[1] * ms[1] - ms[2] * ms[0])
+    print("Коэффициенты ортогонального многочлена:")
+    print("\ta1 = {}\n\ta2 = {}".format(a1, a2))
+    d = a1 * a1 - 4 * a2
+    if d <= 0:
+        print("Упс! Что-то пошло не так!")
+        exit()
+    x1 = (-a1 + math.sqrt(d)) / 2
+    x2 = (-a1 - math.sqrt(d)) / 2
+    print("Узлы КФ:")
+    print("\tx1 = {}\n\tx2 = {}".format(x1, x2))
+    coef1 = 1 / (x1 - x2) * (ms[1] - x2 * ms[0])
+    coef2 = 1 / (x2 - x1) * (ms[1] - x1 * ms[0])
+    print("Коэффициенты КФ:")
+    print("\tA1 = {}\n\tA2 = {}".format(coef1, coef2))
+    return coef1 * f(x1) + coef2 * f(x2)
+
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import gausslikeqf
 import compoundgaussqf
 import math
 
+from gausslikeqf import gauss_qf2
+
 def f(x):
-    return math.exp(x)
-def ro(x):
     return math.sin(x)
+def ro(x):
+    return math.exp(x)
+
+def pol_deg3(x):
+    return x ** 3
 
 def fi(x):
     return ro(x) * f(x)
@@ -12,11 +18,16 @@ def fi(x):
 def fi_integral(a, b):
     return 1 / 2 * (math.sin(b) * math.exp(b) - math.cos(b) * math.exp(b) - math.sin(a) * math.exp(a) + math.cos(a) * math.exp(a))
 
+MOMENTS = [lambda x: math.exp(x),
+        lambda x: (x - 1) * math.exp(x),
+        lambda x: (x ** 2 - 2 * x + 2) * math.exp(x),
+        lambda x: (x ** 3 - 3 * x ** 2 + 6 * x - 6) * math.exp(x)]
+
 print("Лабораторная работа №5")
 print("Приближенное вычисление интегралов при помощи КФ НАСТ")
 print("Вариант №8")
-print("Вес: ro(x) = sin(x)")
-print("Функция: f(x) = exp(x)")
+print("Вес: ro(x) = exp(x)")
+print("Функция: f(x) = sin(x)")
 status = "y"
 while status == "y":
     a, b = map(float, input("Введите пределы интегрирования [a, b]: ").split(","))
@@ -34,4 +45,18 @@ while status == "y":
     res = compoundgaussqf.gauss_qf(fi, a, b, n, m)
     print("Результат СКФ Гаусса: {:.12f}".format(res))
     print("Абсолютная фактическая погрешность: {}".format(abs(fi_integral(a, b) - res)))
+    status = input("Хотите продолжить? y/[n] ")
+
+print("Приближенное вычисление интегралов при помощи КФ типа Гаусса")
+status = "y"
+while status == "y":
+    a, b = map(float, input("Введите пределы интегрирования [a, b]: ").split(","))
+    print("Проверка формулы на полиноме x^3:")
+    res = gausslikeqf.gauss_qf2(pol_deg3, a, b, MOMENTS)
+    print("\tРезультат: {:.12f}".format(res))
+    print("\tАбсолютная фактическая погрешность:", abs(res - (MOMENTS[3](b) - MOMENTS[3](a))))
+    print("Вычисление интеграла ro(x)*f(x)")
+    res = gausslikeqf.gauss_qf2(f, a, b, MOMENTS)
+    print("Результат: {:.12f}".format(res))
+    print("Абсолютная фактическая погрешнось:", abs(fi_integral(a, b) - res))
     status = input("Хотите продолжить? y/[n] ")
