3293fb742b2daa553e267c07efc286937d4b481e
Al.Romanov <raa_2001@mail.ru>
2021-12-13 22:12:42 +0300

    gauss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import gaussqf
-from math import cos, pi
+from math import cos, pi, exp, sin
 
 def polynom_str(polynom):
     polynom_with_x = [str(k) + "*x^" + str(deg) for deg, k in enumerate(polynom)]
@@ -21,8 +21,12 @@ def polynom_integral(polynom, a, b):
 def gauss_f(x):
     return cos(x * x)
 
+def meler_f(x):
+    return exp(x) * sin(x * x)
+
 print("Лабораторная работа №5")
 print("Вычисление интегралов при помощи КФ Гаусса")
+print("Лабораторная работа №8")
 print("Коэффициенты и узлы КФ Гаусса")
 for n in range(1, 9):
     print("N = {}:".format(n))
@@ -33,18 +37,43 @@ for n in range(1, 9):
         print("\t{}".format(units[i]).ljust(number_len), "<-> {}".format(coefficients[i]))
 
 print("Проверка точности КФ Гаусса на многочленах наивысшей степени")
-N_UNITS = (3, 4, 5)
 a = []
 n_test = 1
-for n in N_UNITS:
+for n in (3, 4, 5):
     print("{})".format(n_test), end="")
     n_test += 1
     print("\tКоличество узлов: {}".format(n))
     print("\tНаивысшая степень многочлена: {}".format(2 * n - 1))
     polynom = test_polynom_deg(2 * n - 1)
     print("\tМногочлен:", polynom_str(polynom))
-    actual_integral = polynom_integral(polynom, -1, 0.1)
+    actual_integral = polynom_integral(polynom, -1, 1)
     print("\tИнтеграл:", actual_integral)
-    gauss_integral = gaussqf.gauss_qf(lambda x: gaussqf.compute_polynom(polynom, x), -1, 0.1, n)
+    gauss_integral = gaussqf.gauss_qf(lambda x: gaussqf.compute_polynom(polynom, x), -1, 1, n)
     print("\tИнтеграл по КФ Гаусса:", gauss_integral)
     print("\tАбсолютная фактическая погрешность:", abs(gauss_integral - actual_integral))
+
+print("Вычисление интеграла функции f(x) = cos(x^2) при помощи КФ Гаусса")
+n_test = 1
+status = "y"
+while status == "y":
+    a, b = map(float, input("Введите пределы интегрирования [a, b]: ").split(","))
+    for n in (3, 6, 7, 8):
+        print("{})".format(n_test), end="")
+
+        n_test += 1
+        print("\tКоличество узлов: {}".format(n))
+        units = gaussqf.get_units(n)
+        coefficients = gaussqf.get_coefficients(units)
+        units = [a + (b - a) / 2 * (x_k + 1) for x_k in units]
+        coefficients = [coef * (b - a) / 2 for coef in coefficients]
+        number_len = max([len(str(unit)) + 1 for unit in units])
+        print("\tУзлы и коэффициенты:")
+        for i in range(len(units)):
+            print("\t{}".format(units[i]).ljust(number_len), "<-> {}".format(coefficients[i]))
+        print("Результат:", gaussqf.gauss_qf(gauss_f, a, b, n))
+    status = input("Вы хотите продолжить? [y/N]:")
+
+print("Вычисление интеграла функции exp(x) * sin(x^2) / sqrt(1 - x^2) при помощи КФ Мелера")
+print("Вес ro(x) = 1 / sqrt(1 - x^2)")
+print("f(x) =  exp(x) * sin(x^2)")
+
