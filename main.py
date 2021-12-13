d0e6d40976dae175348f67f53b83eec2516c867a
Al.Romanov <raa_2001@mail.ru>
2021-12-13 18:48:13 +0300

    hz

--- main.py
+++ main.py
@@ -1,50 +1,62 @@
-import gaussqf
-from math import cos, pi
+import gausslikeqf
+import compoundgaussqf
+import math
 
-def polynom_str(polynom):
-    polynom_with_x = [str(k) + "*x^" + str(deg) for deg, k in enumerate(polynom)]
-    return " + ".join(polynom_with_x)
+from gausslikeqf import gauss_qf2
 
-def test_polynom_deg(deg):
-    return [i + 1 for i in range(deg + 1)]
+def f(x):
+    return math.sin(x)
+def ro(x):
+    return math.exp(x)
 
-def polynom_integral(polynom, a, b):
-    polynom_integral = polynom
-    integral_a = 0
-    integral_b = 0
-    for deg in range(len(polynom_integral)):
-        integral_a += a ** (deg + 1) * polynom_integral[deg] / (deg + 1)
-        integral_b += b ** (deg + 1) * polynom_integral[deg] / (deg + 1)
-    return integral_b - integral_a
+def pol_deg3(x):
+    return x ** 3
 
+def fi(x):
+    return ro(x) * f(x)
 
-def gauss_f(x):
-    return cos(x * x)
+def fi_integral(a, b):
+    return 1 / 2 * (math.sin(b) * math.exp(b) - math.cos(b) * math.exp(b) - math.sin(a) * math.exp(a) + math.cos(a) * math.exp(a))
+
+MOMENTS = [lambda x: math.exp(x),
+        lambda x: (x - 1) * math.exp(x),
+        lambda x: (x ** 2 - 2 * x + 2) * math.exp(x),
+        lambda x: (x ** 3 - 3 * x ** 2 + 6 * x - 6) * math.exp(x)]
 
 print("Лабораторная работа №5")
-print("Вычисление интегралов при помощи КФ Гаусса")
-print("Коэффициенты и узлы КФ Гаусса")
-for n in range(1, 9):
-    print("N = {}:".format(n))
-    units = gaussqf.get_units(n)
-    coefficients = gaussqf.get_coefficients(units)
+print("Приближенное вычисление интегралов при помощи КФ НАСТ")
+print("Вариант №8")
+print("Вес: ro(x) = exp(x)")
+print("Функция: f(x) = sin(x)")
+status = "y"
+while status == "y":
+    a, b = map(float, input("Введите пределы интегрирования [a, b]: ").split(","))
+    n = int(input("Введите число узлов КФ (n): "))
+    m = int(input("Введите число отрезков деления [{}, {}] (m): ".format(a, b)))
+    h = (b - a) / m
+    print("Узлы и коэффициенты исходной КФ Гаусса:")
+    units = compoundgaussqf.get_units(n)
+    coefficients = compoundgaussqf.get_coefficients(units)
+    units = [a + (b - a) / 2 * (x_k + 1) for x_k in units]
+    coefficients = [coef * (b - a) / 2 for coef in coefficients]
     number_len = max([len(str(unit)) + 1 for unit in units])
     for i in range(len(units)):
         print("\t{}".format(units[i]).ljust(number_len), "<-> {}".format(coefficients[i]))
+    res = compoundgaussqf.gauss_qf(fi, a, b, n, m)
+    print("Результат СКФ Гаусса: {:.12f}".format(res))
+    print("Абсолютная фактическая погрешность: {}".format(abs(fi_integral(a, b) - res)))
+    status = input("Хотите продолжить? y/[n] ")
 
-print("Проверка точности КФ Гаусса на многочленах наивысшей степени")
-N_UNITS = (3, 4, 5)
-a = []
-n_test = 1
-for n in N_UNITS:
-    print("{})".format(n_test), end="")
-    n_test += 1
-    print("\tКоличество узлов: {}".format(n))
-    print("\tНаивысшая степень многочлена: {}".format(2 * n - 1))
-    polynom = test_polynom_deg(2 * n - 1)
-    print("\tМногочлен:", polynom_str(polynom))
-    actual_integral = polynom_integral(polynom, -1, 0.1)
-    print("\tИнтеграл:", actual_integral)
-    gauss_integral = gaussqf.gauss_qf(lambda x: gaussqf.compute_polynom(polynom, x), -1, 0.1, n)
-    print("\tИнтеграл по КФ Гаусса:", gauss_integral)
-    print("\tАбсолютная фактическая погрешность:", abs(gauss_integral - actual_integral))
+print("Приближенное вычисление интегралов при помощи КФ типа Гаусса")
+status = "y"
+while status == "y":
+    a, b = map(float, input("Введите пределы интегрирования [a, b]: ").split(","))
+    print("Проверка формулы на полиноме x^3:")
+    res = gausslikeqf.gauss_qf2(pol_deg3, a, b, MOMENTS)
+    print("\tРезультат: {:.12f}".format(res))
+    print("\tАбсолютная фактическая погрешность:", abs(res - (MOMENTS[3](b) - MOMENTS[3](a))))
+    print("Вычисление интеграла ro(x)*f(x)")
+    res = gausslikeqf.gauss_qf2(f, a, b, MOMENTS)
+    print("Результат: {:.12f}".format(res))
+    print("Абсолютная фактическая погрешнось:", abs(fi_integral(a, b) - res))
+    status = input("Хотите продолжить? y/[n] ")
